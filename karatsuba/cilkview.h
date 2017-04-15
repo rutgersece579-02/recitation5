8530201de9867385570071f0145fc059d7e346d0
rutgersece579-02 <appliedparallelrutgers17@gmail.com>
2017-04-15 12:48:28 -0400

    Update cilkview.h

--- karatsuba/cilkview.h
+++ karatsuba/cilkview.h
@@ -1,25 +1,50 @@
 /* cilkview.h                  -*-C++-*-
  *
  *************************************************************************
- *                         INTEL CONFIDENTIAL
  *
- * Copyright (C) 2010-2017 Intel Corporation.  All Rights Reserved.
- *
- *  The source code contained or described herein and all documents related
- *  to the source code ("Material") are owned by Intel Corporation or its
- *  suppliers or licensors.  Title to the Material remains with Intel
- *  Corporation or its suppliers and licensors.  The Material is protected
- *  by worldwide copyright laws and treaty provisions.  No part of the
- *  Material may be used, copied, reproduced, modified, published, uploaded,
- *  posted, transmitted, distributed, or disclosed in any way without
- *  Intel's prior express written permission.
- *
- *  No license under any patent, copyright, trade secret or other
- *  intellectual property right is granted to or conferred upon you by
- *  disclosure or delivery of the Materials, either expressly, by
- *  implication, inducement, estoppel or otherwise.  Any license under such
- *  intellectual property rights must be express and approved by Intel in
- *  writing.
+ * Copyright (C) 2010-2016, Intel Corporation
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * *********************************************************************
+ * 
+ * PLEASE NOTE: This file is a downstream copy of a file mainitained in
+ * a repository at cilkplus.org. Changes made to this file that are not
+ * submitted through the contribution process detailed at
+ * http://www.cilkplus.org/submit-cilk-contribution will be lost the next
+ * time that a new version is released. Changes only submitted to the
+ * GNU compiler collection or posted to the git repository at
+ * https://bitbucket.org/intelcilkruntime/intel-cilk-runtime.git are
+ * not tracked.
+ * 
+ * We welcome your contributions to this open source project. Thank you
+ * for your assistance in helping us improve Cilk Plus.
  *
  **************************************************************************/
 
